4b4c1d8f3cc70bd463143311fec50840fe4def58
ulissesc <torquatro@gmail.com>
2013-11-16 09:32:11 -0200

    desabilitado assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,10 @@ module SitesCms
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.enabled = false
+    
     config.railties_order = [ :all, ComfortableMexicanSofa::Engine ]
+
   end
 end
 
